059b782d1972d64cce3c80d2536edfca4f02ba49
yuusf-muibi <muibiyusuf@hotmail.com>
2024-08-14 21:13:35 +0100

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -20,15 +20,12 @@ def data_cacher(method: Callable) -> Callable:
         '''The wrapper function for caching the output.
         '''
         redis_store.incr(f'count:{url}')
-        
-        cached_result = redis_store.get(f'result:{url}')
-        if cached_result:
-            return cached_result.decode('utf-8')
-        
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
         result = method(url)
-        
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
-        
         return result
     return invoker
 
@@ -38,5 +35,4 @@ def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
-    response = requests.get(url)
-    return response.text
+    return requests.get(url).text
